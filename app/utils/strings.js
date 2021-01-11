79ed0414d7007c35681b9b2abb0781c960ee9144
Varasney <varasney.a@kuliza.com>
2021-01-11 18:49:16 +0530

    Adds constant strings

---
+++ app/utils/strings.js
@@ -0,0 +1,9 @@
+export const stringMessages = {
+    budgetAlert: "Total Budget Alert",
+    moreThan: `Your total Monthly Amount should not be more then`,
+    minBudget:"Total Budget Must be greater than "
+
+}
+export const strings = (id) => {
+    return stringMessages[id]
+}
