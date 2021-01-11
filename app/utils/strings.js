971fa916503605f606a694810bca4e88c6c01394
Varasney <varasney.a@kuliza.com>
2021-01-11 19:31:34 +0530

    adds string message

--- app/utils/strings.js
+++ app/utils/strings.js
@@ -1,7 +1,8 @@
 export const stringMessages = {
     budgetAlert: "Total Budget Alert",
     moreThan: `Your total Monthly Amount should not be more then`,
-    minBudget:"Total Budget Must be greater than "
+    minBudget:"Total Budget Must be greater than ",
+    yourMonthlyBudget:"Enter Your Monthly Budget"
 
 }
 export const strings = (id) => {
